4992f05cc1aec3a4d4bc2eeb8cc72ac199d0a16b
agustiyann <agustiyaninfo@gmail.com>
2020-12-23 14:47:09 +0700

    ReadOnlyProperty Interface

---
+++ src/app/ReadOnlyProperty.kt
@@ -0,0 +1,21 @@
+package app
+
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+class LogReadOnlyProperty(val data: String): ReadOnlyProperty<Any, String> {
+    override fun getValue(thisRef: Any, property: KProperty<*>): String {
+        println("Access property ${property.name} with value $data")
+        return data.toUpperCase()
+    }
+}
+
+class NameWithLog(param: String) {
+    val name: String by LogReadOnlyProperty(param)
+}
+
+fun main() {
+    val nameWithLog = NameWithLog("Agus Tiyan")
+    println(nameWithLog.name)
+    print(nameWithLog.name)
+}
